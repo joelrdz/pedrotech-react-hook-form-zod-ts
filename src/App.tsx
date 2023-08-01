374cbab4f428f1915596707da25fcde085d502fd
Joel Rdz <joelrdzh@gmail.com>
2023-08-01 14:17:19 -0600

    confirmPassword added, refine to precise checking

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { z } from 'zod';
 import './App.css';
 
 function App() {
-  const schema = z.object({
-    firstName: z.string().min(2).max(30),
-    lastName: z.string().min(2).max(30),
-    email: z.string().email(),
-    age: z.number().min(18).max(70),
-    password: z.string().min(5).max(20),
-  });
+  const schema = z
+    .object({
+      firstName: z.string().min(2).max(30),
+      lastName: z.string().min(2).max(30),
+      email: z.string().email(),
+      age: z.number().min(18).max(70),
+      password: z.string().min(5).max(20),
+      confirmPassword: z.string().min(5).max(20),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Passwords do not match',
+      path: ['confirmPassword'],
+    });
 
   return (
     <div>
@@ -22,6 +28,8 @@ function App() {
         <label>Age: </label>
         <input type="number" />
         <label>Password: </label>
+        <input type="password" />
+        <label>Confirm Password: </label>
         <input type="password" />
 
         <br />
